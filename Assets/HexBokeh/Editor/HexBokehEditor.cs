0b0d001b82046fc66bf0b0ee8e794d041df218f9
Keijiro Takahashi <keijiro@Super-MSX.local>
2014-08-18 07:32:41 +0900

    Added the editor script.

---
+++ Assets/HexBokeh/Editor/HexBokehEditor.cs
@@ -0,0 +1,69 @@
+//
+// HexBokeh - A Fast DOF Shader With Hexagonal Apertures
+//
+// Copyright (C) 2014 Keijiro Takahashi
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+//
+// This shader is based on McIntosh's paper "Efficiently Simulating the Bokeh of
+// Polygonal Apertures in a Post-Process Depth of Field Shader". For further
+// details see the paper below.
+//
+// http://ivizlab.sfu.ca/media/DiPaolaMcIntoshRiecke2012.pdf
+//
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(HexBokeh)), CanEditMultipleObjects]
+public class HexBokehEditor : Editor
+{
+    SerializedProperty propFocalLength;
+    SerializedProperty propFocalSize;
+    SerializedProperty propAperture;
+    SerializedProperty propVisualize;
+    SerializedProperty propNearBlur;
+    SerializedProperty propSampleDist;
+
+    void OnEnable()
+    {
+        propFocalLength = serializedObject.FindProperty("focalLength");
+        propFocalSize   = serializedObject.FindProperty("focalSize");
+        propAperture    = serializedObject.FindProperty("aperture");
+        propVisualize   = serializedObject.FindProperty("visualize");
+        propNearBlur    = serializedObject.FindProperty("nearBlur");
+        propSampleDist  = serializedObject.FindProperty("sampleDist");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(propFocalLength);
+        EditorGUILayout.PropertyField(propFocalSize);
+        EditorGUILayout.PropertyField(propAperture);
+        EditorGUILayout.PropertyField(propVisualize, new GUIContent("Visualize CoC"));
+        EditorGUILayout.PropertyField(propNearBlur);
+        EditorGUILayout.PropertyField(propSampleDist, new GUIContent("Sample Distance"));
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
